c8068d6a9a3898b3b299862f68cdd0390d23492b
AlexandrKhmil <oleksandr.khmil2@gmail.com>
2020-04-26 09:44:00 +0300

    last

---
+++ client/src/containers/App/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <main className="container">
+      APP
+      <button className="btn btn-primary">Button</button>
+    </main>
+  );
+}
+
+export default App;
