3a1272593f3a593e524a7f9cd28454a46b1dee94
Alex <oleksandr.khmil2@gmail.com>
2020-04-28 13:47:30 +0300

    heroku test 2

---
+++ client/build/precache-manifest.3488fab1d8a82c8485acc7e889e06def.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b3b58a560bfff8f6998c8c0018abc0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64d1a160e800a297308d",
+    "url": "/static/css/main.1e6611eb.chunk.css"
+  },
+  {
+    "revision": "44877a4343932c87c626",
+    "url": "/static/js/2.6209aa20.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6209aa20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64d1a160e800a297308d",
+    "url": "/static/js/main.25d94fec.chunk.js"
+  },
+  {
+    "revision": "dad4adfa7062cb4d7df7",
+    "url": "/static/js/runtime-main.597b7f10.js"
+  }
+]);
