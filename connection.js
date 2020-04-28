3df8132b2c2971ac798aa9ca5cb6295a3ecbe481
Alex <oleksandr.khmil2@gmail.com>
2020-04-28 13:29:57 +0300

    heroku test 2

--- connection.js
+++ connection.js
@@ -1,5 +1,8 @@
 const pgp = require('pg-promise')();
 const config = require('config');
-const db = pgp(config.get('db'));
+
+pgp.pg.defaults.ssl = process.env.DATABASE_URL && true;
+pgp.pg.defaults.poolSize = 10;
+const db = pgp(process.env.DATABASE_URL || config.get('db'));
 
 module.exports = db;
--- index.js
+++ index.js
@@ -1,50 +1,55 @@
 const express = require('express');
 const {spawn} = require('child_process');
 const app = express();
-const port = 3001;
+const path = require('path');
+const port = process.env.PORT || 3001;
 
 app.use(express.json({ extended: true }));
 
 app.use('/api/account', require('./routes/account.routes'));
 app.use('/api/algorithm', require('./routes/algorithm.routes'));
 
-app.get('/1', (req, res) => {
-  var dataToSend;
-  // spawn new child process to call the python script
-  const python = spawn('python', ['script.py']);
+app.use('/', express.static(path.join(__dirname, 'client', 'build')));
+app.get('*', (req, res) => 
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+
+// app.get('/1', (req, res) => {
+//   var dataToSend;
+//   // spawn new child process to call the python script
+//   const python = spawn('python', ['script.py']);
   
-  // collect data from script
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
-    dataToSend = data.toString();
-  });
+//   // collect data from script
+//   python.stdout.on('data', function (data) {
+//     console.log('Pipe data from python script ...');
+//     dataToSend = data.toString();
+//   });
 
-  // in close event we are sure that stream from child process is closed
-  python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    res.send(dataToSend);
-  });
-});
+//   // in close event we are sure that stream from child process is closed
+//   python.on('close', (code) => {
+//     console.log(`child process close all stdio with code ${code}`);
+//     // send data to browser
+//     res.send(dataToSend);
+//   });
+// });
 
-app.get('/2', (req, res) => {
-  var dataToSend;
-  // spawn new child process to call the python script
-  const python = spawn('python', ['script2.py', 'node.js', 'python']);
+// app.get('/2', (req, res) => {
+//   var dataToSend;
+//   // spawn new child process to call the python script
+//   const python = spawn('python', ['script2.py', 'node.js', 'python']);
   
-  // collect data from script
-  python.stdout.on('data', function (data) {
-    console.log('Pipe data from python script ...');
-    dataToSend = data.toString();
-  });
+//   // collect data from script
+//   python.stdout.on('data', function (data) {
+//     console.log('Pipe data from python script ...');
+//     dataToSend = data.toString();
+//   });
 
-  // in close event we are sure that stream from child process is closed
-  python.on('close', (code) => {
-    console.log(`child process close all stdio with code ${code}`);
-    // send data to browser
-    res.send(dataToSend);
-  });
-});
+//   // in close event we are sure that stream from child process is closed
+//   python.on('close', (code) => {
+//     console.log(`child process close all stdio with code ${code}`);
+//     // send data to browser
+//     res.send(dataToSend);
+//   });
+// });
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
