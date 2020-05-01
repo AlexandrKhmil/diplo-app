cb3a5482add75ecd9c947d590ef976d8cdd5f543
Alex <oleksandr.khmil2@gmail.com>
2020-05-01 13:38:58 +0300

    python modules heroku test

--- python_modules/linear_regression/__init__.py
+++ python_modules/linear_regression/__init__.py
@@ -1,11 +1,11 @@
-from linear_regression import least_squares, predict
-import sys, json #, numpy as np
+# from linear_regression import least_squares, predict
+# import sys, json #, numpy as np
 
-#Read data from stdin
-def read_in():
-  lines = sys.stdin.readlines()
-  #Since our input would only be having one line, parse our JSON data from that
-  return json.loads(lines[0])
+# #Read data from stdin
+# def read_in():
+#   lines = sys.stdin.readlines()
+#   #Since our input would only be having one line, parse our JSON data from that
+#   return json.loads(lines[0])
 
 def main():
   #get our data as an array from read_in()
