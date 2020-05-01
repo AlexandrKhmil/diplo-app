befda545267a329b9cab4c89df87c1d9f336f931
Alex <oleksandr.khmil2@gmail.com>
2020-05-01 14:51:52 +0300

    python modules heroku test

--- python_modules/linear_regression/__init__.py
+++ python_modules/linear_regression/__init__.py
@@ -7,6 +7,21 @@ def read_in():
 def dot(v, w):
   return sum(v_i * w_i for v_i, w_i in zip(v, w))
 
+def sum_of_squares(v):
+  return dot(v, v)
+
+def mean(x):
+  return sum(x) / len(x)
+
+def de_mean(x):
+  x_bar = mean(x)
+  return [x_i - x_bar for x_i in x]
+
+def variance(x):
+  n = len(x)
+  deviations = de_mean(x)
+  return sum_of_squares(deviations) / (n - 1)
+
 def main():
   lines = read_in()
   data = lines['data']
