005bc31264b4e15c148c1c9036f6b8a2525cc132
Alex <oleksandr.khmil2@gmail.com>
2020-05-01 14:44:22 +0300

    python modules heroku test

--- python_modules/linear_regression/__init__.py
+++ python_modules/linear_regression/__init__.py
@@ -6,7 +6,13 @@ def read_in():
 
 def main():
   lines = read_in()
-  print(lines['data'])
+  data = lines['data']
+  forward = int(lines['forward'])
+  arr = [[], []]
+  for i in range(0, len(data) - 1):
+    arr[0].append(data[i])
+    arr[1].append(data[i + 1])
+  print(arr[0])
 
 if __name__ == '__main__':
   main()
