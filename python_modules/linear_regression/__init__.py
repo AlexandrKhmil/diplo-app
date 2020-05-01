9b91b6701ed8077df7e78c874fe4d1cb4381eda9
Alex <oleksandr.khmil2@gmail.com>
2020-05-01 13:55:57 +0300

    python modules heroku test

--- python_modules/linear_regression/__init__.py
+++ python_modules/linear_regression/__init__.py
@@ -1,28 +1,31 @@
-from linear_regression import least_squares, predict
-import sys, json #, numpy as np
+# from linear_regression import least_squares, predict
+# import sys, json #, numpy as np
 
-# #Read data from stdin
-def read_in():
-  lines = sys.stdin.readlines()
-  #Since our input would only be having one line, parse our JSON data from that
-  return json.loads(lines[0])
+from q import f
+
+# # #Read data from stdin
+# def read_in():
+#   lines = sys.stdin.readlines()
+#   #Since our input would only be having one line, parse our JSON data from that
+#   return json.loads(lines[0])
 
 def main():
   #get our data as an array from read_in()
   # print([12321321])
-  lines = read_in()
-  data = lines['data']
-  forward = int(lines['forward'])
-  arr = [[], []]
-  for i in range(0, len(data) - 1):
-    arr[0].append(data[i])
-    arr[1].append(data[i + 1])
-  alpha, beta = least_squares(arr[0], arr[1]) 
-  result = [data[len(data) - 1]]
-  for i in range(0, forward):
-    result.append(predict(alpha, beta, result[i]))
-  result.pop(0)
-  print(result)
+  # lines = read_in()
+  # data = lines['data']
+  # forward = int(lines['forward'])
+  # arr = [[], []]
+  # for i in range(0, len(data) - 1):
+  #   arr[0].append(data[i])
+  #   arr[1].append(data[i + 1])
+  # alpha, beta = least_squares(arr[0], arr[1]) 
+  # result = [data[len(data) - 1]]
+  # for i in range(0, forward):
+  #   result.append(predict(alpha, beta, result[i]))
+  # result.pop(0)
+  # print(result)
+  return([f()])
 
 #start process
 if __name__ == '__main__':
