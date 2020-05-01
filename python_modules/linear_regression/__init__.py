149ae8e67fd748555da2ff993ebf8d6c24f2163b
Alex <oleksandr.khmil2@gmail.com>
2020-05-01 14:38:48 +0300

    python modules heroku test

--- python_modules/linear_regression/__init__.py
+++ python_modules/linear_regression/__init__.py
@@ -1,5 +1,12 @@
+import sys, json, math
+
+def read_in():
+  lines = sys.stdin.readlines()
+  return json.loads(lines[0])
+
 def main():
-  print([6666])
+  lines = read_in()
+  print(lines)
 
 if __name__ == '__main__':
   main()
