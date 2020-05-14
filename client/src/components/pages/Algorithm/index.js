d08c8ce49e103a4978ffe512cce06bb298f08e79
Alex <oleksandr.khmil2@gmail.com>
2020-05-14 11:41:48 +0300

    Algo Pages

--- client/src/components/pages/Algorithm/index.js
+++ client/src/components/pages/Algorithm/index.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import { algoGetInfo } from '../../../functions/algorithm';
+import styles from './styles.module.css';
 
 const Algorithm = (props) => {
   const { link } = props.match.params;
-
   const [algo, setAlgo] = useState({}); 
   const [isLoaded, setIsLoaded] = useState(false);
 
@@ -19,9 +19,22 @@ const Algorithm = (props) => {
     <main>
       <div className="container mt-5 mb-4">
         <div className="row">
-          <div className="">
 
+          <div className="col-12">
+            <h1 className="mb-3">{algo.title && algo.title}</h1>
+
+            <div className="card card-body border-primary">
+              <div className="row">
+                <div className="col-6">
+                  <img className={styles.img} src={algo.img_url || 'https://i.imgur.com/Bb2evGw.jpg'} alt="Algorithm" />
+                </div>
+                <div className="col-6">
+                  {algo.short_desc ? algo.short_desc : 'Описание отсутствует...'}
+                </div>
+              </div>
+            </div>
           </div>
+
         </div>
       </div>
     </main>
@@ -29,16 +42,3 @@ const Algorithm = (props) => {
 };
 
 export default Algorithm;
-
-
-// desc: "Эта опиция сущесвтует чтобы проверить исполнение Python файлов"
-// ​
-// id: 1
-// ​
-// img_url: "https://www.innovation.ca/sites/default/files/styles/large_2/public/algorithms.png"
-// ​
-// link: "linear_regression"
-// ​
-// short_desc: "Some quick example text to build on the card title and make up the bulk of the card"
-// ​
-// title: "Linear Regression"
