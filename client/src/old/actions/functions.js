731f8c32a299c28f8680fbd62a92e511f33a2f7a
Alex <oleksandr.khmil2@gmail.com>
2020-05-28 16:06:39 +0300

    delete old

--- client/src/old/actions/functions.js
+++
@@ -1,6 +0,0 @@
-export const jsonRequest = (headers, body) => { 
-  const config = { headers: { 'Content-Type': 'application/json' } };
-  config.headers = { ...config.headers, ...headers };
-  body = body && JSON.stringify(body);
-  return { config, body };
-};
